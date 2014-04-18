77b3033f070b7291eb0f0da1b429a5a056fbcfba
unknown <doyin@DOYIN-CN.cn.oracle.com>
2014-04-18 13:10:57 +0800

    1

---
+++ src/readme.txt
@@ -0,0 +1,7 @@
+Files in this directory will be served under your http://server/hudson/userContent/
+1
+2
+3
+4
+5
+test
---
+++ src/wcp-runInstaller.sh
@@ -0,0 +1,1 @@
+wc/Disk1/runInstaller -jreLoc /home/oracle/jrockit-jdk1.6.0_37-R28.2.5-4.1.0 -silent -responseFile /home/oracle/dev/eclipse/workspace/wcinstall/wcp.rsp
